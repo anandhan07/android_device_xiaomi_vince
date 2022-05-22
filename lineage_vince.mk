fc62bc3438b56b6d501f468dc02ff0b1b08be43d
anandhan07 <anandhasayanan007jb@gmail.com>
2022-05-22 13:53:45 +0530

    vince: Bringup Forklineage-OS
     * Bring back faceunlock and Quicktap flags
    
    Signed-off-by: anandhan07 <anandhasayanan007jb@gmail.com>

--- lineage_vince.mk
+++ lineage_vince.mk
@@ -28,7 +28,11 @@ $(call inherit-product, device/xiaomi/vince/device.mk)
 # Inherit some common lineage-OS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Forklineage-OS Flags
+TARGET_FLOS := true
 TARGET_BOOT_ANIMATION_RES := 720
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_SUPPORTS_QUICK_TAP := true
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := vince
